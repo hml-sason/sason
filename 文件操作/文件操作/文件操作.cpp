d6d231c24c9e58de89a6be47be119718ee7c1287
hml-sason <60804311+hml-sason@users.noreply.github.com>
2020-05-09 22:25:41 +0800

    Add files via upload

---
+++ 文件操作/文件操作/文件操作.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<iomanip>
+#include<math.h>
+#include<stdio.h>
+#include<float.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include<ctype.h>
+#include<algorithm>
+#include<fstream>
+using namespace std;
+int main()
+{
+	int a[1001][1001];
+	ofstream fout("out.txt");
+	for (int i = 1; i <= 999; i++)
+	{
+		for (int j = 1; j <= 999; j++)
+		{
+			fout << 6;
+		}
+		fout << endl;
+	}
+	fout.close();
+	return 0;
+}
