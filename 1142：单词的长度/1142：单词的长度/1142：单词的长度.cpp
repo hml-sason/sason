d4183985989f12ebabe074b193ebdd016e9bc630
hml-sason <60804311+hml-sason@users.noreply.github.com>
2020-05-09 22:27:23 +0800

    Add files via upload

---
+++ 1142：单词的长度/1142：单词的长度/1142：单词的长度.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+#include<string.h>
+using namespace std;
+int main()
+{
+	char a[1001];
+	
+	return 0;
+}
