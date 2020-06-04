f52a06d25a22bd12f2beb311552ebf30608ad11d
hml-sason <60804311+hml-sason@users.noreply.github.com>
2020-06-04 21:56:45 +0800

    Add files via upload

---
+++ 快速排序.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<cstring>
+using namespace std;
+int a[10] = { 31,11,49,15,36,47,5,91,88,10 };
+void qsort(int l, int r)
+{
+	int mid = a[(l + r) / 2];
+	int i = l, j = r;
+	while (i <= j)
+	{
+		while (a[i] < mid) i++;
+		while (a[j] > mid) j--;
+
+		if (i <= j)
+		{
+			swap(a[i++], a[j--]);
+		}
+	}
+	if (l < j) qsort(l, j);
+	if (i < r) qsort(i, r);
+}
+int main()
+{
+	qsort(0, 9);
+	for (int i = 0; i < 9; i++) cout << a[i] << ' '; 
+	return 0;
+}
